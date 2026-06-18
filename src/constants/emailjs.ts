1d82c11318cd876689a813dc13a86d0a2d716653
childerx <iabu007@st.ug.edu.gh>
2026-06-18 23:39:58 +0000

    update

--- src/constants/emailjs.ts
+++ src/constants/emailjs.ts
@@ -1,3 +1,3 @@
-export const EMAILJS_SERVICE_ID = "service_e7x5ogv";
-export const EMAILJS_TEMPLATE_ID = "template_s48hprb";
-export const EMAILJS_PUBLIC_KEY = "4yxSqp-h0THsKOKDV";
+export const EMAILJS_SERVICE_ID = "service_38e5nra";
+export const EMAILJS_TEMPLATE_ID = "template_ouvjjei";
+export const EMAILJS_PUBLIC_KEY = "sRU9UqV8gWXEwUw5T";
